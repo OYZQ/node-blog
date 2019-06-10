c1d82109e3cfed958ded26bf7d1ce6d9f0746b35
oyzq_glb <ouyangzhiqiang98_glb@gmail.com>
2019-06-10 09:41:14 +0800

    初始化路由

--- blog-1/app.js
+++ blog-1/app.js
@@ -1,7 +1,32 @@
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
+
 const serverHandle = (req,res) => {
     // 设置返回格式 JSON
     res.setHeader('Content-type','application/json')
+    
+    // 处理blog路由
+    const blogData = handleBlogRouter(req,res)
+    if (blogData) {
+        res.end(
+            JSON.stringify(blogData)
+        )
+        return
+    }
 
+    // 处理user 路由
+    const userData = handleUserRouter(req,res) 
+    if (userData) {
+        res.end(
+            JSON.stringify(userData)
+        )
+        return
+    }
+
+    // 未命中路由，返回404
+    res.writeHead(404,{"Content-type":"text/plain"})
+    res.write("404 Not Found\n")
+    res.end()
 }
 
 module.exports = serverHandle
